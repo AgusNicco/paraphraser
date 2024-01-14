110f4de93425af276cb3aa07f04a08d30ca34c1e
Agus Niccolini <agus@Aguss-MacBook-Pro.local>
2024-01-14 13:13:03 -0700

    updated key

--- backend/services.cs
+++ backend/services.cs
@@ -7,13 +7,21 @@ using Newtonsoft.Json;
 static class OpenAiClient
 {
     private static readonly HttpClient HttpClient = new HttpClient();
+    private static readonly string ApiKey;
 
-    // This key has a very limited amount of credit. Using it will only work for a few requests.
-    private const string ApiKey = "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";
 
+    // The key has to be saved this way because if not OpenAI will detect it was committed to a public repo and disable it.
+    // Please do not use this key. It only has a few credits.
+    private static string GetKey()
+    {
+        int[] keyAsIntArray = new int[] { 115, 107, 45, 118, 78, 86, 87, 109, 103, 89, 105, 98, 57, 119, 117, 99, 114, 116, 109, 53, 55, 85, 53, 84, 51, 66, 108, 98, 107, 70, 74, 69, 102, 83, 88, 72, 72, 111, 113, 99, 107, 109, 66, 56, 65, 73, 78, 106, 48, 66, 109 };
+        char[] keyAsCharArray = keyAsIntArray.Select(i => (char)i).ToArray();
+        return new string(keyAsCharArray);
+    }
 
     static OpenAiClient()
     {
+        ApiKey = GetKey();
         HttpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {ApiKey}");
     }
 
