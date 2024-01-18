835ea990e7334ab4dd21bac9f8b64d16cc2cd543
Agus Niccolini <agus@Aguss-MacBook-Pro.local>
2024-01-18 12:50:15 -0700

    improved ui

--- frontend/script.js
+++ frontend/script.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', async function (event) {
     // get user input
     const textInput = document.getElementById('textInput').value;
     const resultsContainer = document.getElementById('result');
-    resultsContainer.innerHTML = 'Processing request. Results will show up here once done. 30s-60s processing time.'; 
+    resultsContainer.innerHTML = 'Processing request. 10s-60s processing time.'; 
     let paraphrasedText = await svc.paraphrase(textInput);
     console.log(paraphrasedText);
     displayWithParagraphs(paraphrasedText, 'result');
@@ -17,6 +17,7 @@ form.addEventListener('submit', async function (event) {
 function displayWithParagraphs(text, containerId) {
     const container = document.getElementById(containerId);
     container.innerHTML = ''; 
+    container.style.display = 'block';
     const paragraphs = text.split('\n\n');
     paragraphs.forEach((paragraph, paragraphIndex) => {
         const p = document.createElement('p');
