ee5aa531b81c81f93e43c43b460a9d8d43e18d79
Agus Niccolini <agus@Aguss-MacBook-Pro.local>
2024-01-18 12:56:19 -0700

    fixed small bug

--- frontend/script.js
+++ frontend/script.js
@@ -7,6 +7,7 @@ form.addEventListener('submit', async function (event) {
     // get user input
     const textInput = document.getElementById('textInput').value;
     const resultsContainer = document.getElementById('result');
+    resultsContainer.style.display = 'block';
     resultsContainer.innerHTML = 'Processing request. 10s-60s processing time.'; 
     let paraphrasedText = await svc.paraphrase(textInput);
     console.log(paraphrasedText);
@@ -17,7 +18,6 @@ form.addEventListener('submit', async function (event) {
 function displayWithParagraphs(text, containerId) {
     const container = document.getElementById(containerId);
     container.innerHTML = ''; 
-    container.style.display = 'block';
     const paragraphs = text.split('\n\n');
     paragraphs.forEach((paragraph, paragraphIndex) => {
         const p = document.createElement('p');
